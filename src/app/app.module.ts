61fe259d42df16ecbe8b1795c5a67262f69a01be
Kuldeep Bhojak <kuldeep.bhojak@gmail.com>
2022-07-14 09:22:39 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
